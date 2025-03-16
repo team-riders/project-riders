aa241f5ab13f21c5cd24865a0ec48a9a62bcd476
AppelJoosh <joshcauson@gmail.com>
2025-03-16 19:31:36 +1100

    started work on vehicle
    created BaseVehicle.cs, BaseInputs.cs, KeyboardInput.cs, also a handful of folders

---
+++ Assets/Scripts/VehicleSystems/BaseVehicle.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class BaseVehicle : MonoBehaviour
+{
+    public struct Stats
+    {
+        // figure out what stats we need
+    }
+
+    // figure out methods we need, refer to ArcadeKart.cs from karting microgame as startpoint
+}
