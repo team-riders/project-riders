ec7a65d7e278484ef09d8d927038f076642f6cf7
AppelJoosh <joshcauson@gmail.com>
2025-03-23 13:42:03 +1100

    attempt at jump implementation
    attempted jump implementation, need to mess with board class to figure out why things aren't working. maybe make some methods virtual

--- Assets/Scripts/VehicleSystems/BoardVehicle.cs
+++ Assets/Scripts/VehicleSystems/BoardVehicle.cs
@@ -9,26 +9,29 @@ namespace Assets.Scripts.VehicleSystems
 {
     class BoardVehicle : BaseVehicle
     {
-        // replace base stats
-        public new BaseVehicle.Stats baseStats = new BaseVehicle.Stats
-        {
-            TopSpeed = 50f,
-            Acceleration = 10f,
-            AccelerationCurve = 4f,
-            Braking = 10f,
-            ReverseAcceleration = 5f,
-            ReverseSpeed = 5f,
-            Steer = 5f,
-            CoastingDrag = 4f,
-            Grip = .95f,
-            AddedGravity = 1f,
-            BoostTopSpeed = 80f,
-            BoostAccel = 15f,
-        };
+        //// replace base stats
+        //public new BaseVehicle.Stats baseStats = new BaseVehicle.Stats
+        //{
+        //    TopSpeed = 50f,
+        //    Acceleration = 10f,
+        //    AccelerationCurve = 4f,
+        //    Braking = 10f,
+        //    ReverseAcceleration = 5f,
+        //    ReverseSpeed = 5f,
+        //    Steer = 5f,
+        //    CoastingDrag = 4f,
+        //    Grip = .95f,
+        //    AddedGravity = 1f,
+        //    BoostTopSpeed = 80f,
+        //    BoostAccel = 15f,
+        //};
 
         [Header("Physical Wheels")]
         [Tooltip("The physical representations of the Kart's wheels.")]
-        public WheelCollider FrontWheel;
-        public WheelCollider BackWheel;
+        public WheelCollider FrontLeftWheel;
+        public WheelCollider FrontRightWheel;
+        public WheelCollider BackLeftWheel;
+        public WheelCollider BackRightWheel;
+        
     }
 }
