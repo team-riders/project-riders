c7c27658410daed7b7426ac9a687dd9764ead26b
AppelJoosh <joshcauson@gmail.com>
2025-04-10 19:30:19 +1000

    remove non-struct content
    doesn't seem to be needed? can run by james

--- Assets/Scripts/RacerInformation.cs
+++ Assets/Scripts/RacerInformation.cs
@@ -3,24 +3,12 @@ using UnityEngine.TextCore.Text;
 
 public class RacerInformation : MonoBehaviour
 {
-    // questionable, find examples elsewhere
+    // questionable, find examples elsewhere or run by James
     [System.Serializable]
     public struct RacerInfo
     {
         public BaseVehicle VehicleSelected;
         public int CharacterSelected;
         public bool IsPlayer;
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
