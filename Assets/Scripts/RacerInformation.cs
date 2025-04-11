3511ed9cdd5049e3e528010530178ec17440a156
AppelJoosh <joshcauson@gmail.com>
2025-04-11 13:42:11 +1000

    remove class, make struct solo

--- Assets/Scripts/RacerInformation.cs
+++ Assets/Scripts/RacerInformation.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
-public class RacerInformation : MonoBehaviour
+// questionable, find examples elsewhere or run by James
+[System.Serializable]
+public struct RacerInformation
 {
-    // questionable, find examples elsewhere or run by James
-    [System.Serializable]
-    public struct RacerInfo
-    {
-        public BaseVehicle VehicleSelected;
-        public int CharacterSelected;
-        public bool IsPlayer;
-    }
+    public BaseVehicle VehicleSelected;
+    public int CharacterSelected;
+    public bool IsPlayer;
 }
