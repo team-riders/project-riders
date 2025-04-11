2664a587fb78849b9890c85081b93bcd9e374c33
AppelJoosh <joshcauson@gmail.com>
2025-04-11 14:38:54 +1000

    separate jump into function
    also seems to have MAGICALLY fixed the problem of jumps not working sometimes

--- Assets/Scripts/VehicleSystems/BaseVehicle.cs
+++ Assets/Scripts/VehicleSystems/BaseVehicle.cs
@@ -455,6 +455,27 @@ public class BaseVehicle : MonoBehaviour
         }
     }
 
+    //jump management
+    // should be satisfactory until we add ramps
+    float Jump(float jumpHold, bool jump, float maxSpeed)
+    {
+        if (jumpHold > 0 && GroundPercent > 0.0f)
+        {
+            JumpCharge = Mathf.Clamp(jumpHold / 120f, 0.5f, 1.0f);
+            Debug.Log("JumpCharge: " + JumpCharge);
+            if (jumpHold > 180)
+            {
+                maxSpeed *= 0.5f;
+            }
+        }
+        if (jump && GroundPercent > 0.0f)
+        {
+            Debug.Log("JumpForce * JumpCharge: " + JumpForce * JumpCharge);
+            Rigidbody.AddForce(Vector3.up * (JumpForce * JumpCharge), ForceMode.Impulse);
+        }
+        return maxSpeed;
+    }
+
     //make virtual?
     void MoveVehicle(bool accelerate, bool brake, float turnInput, bool jump, float jumpHold)
     {
@@ -495,22 +516,8 @@ public class BaseVehicle : MonoBehaviour
         Vector3 fwd = turnAngle * transform.forward;
         Vector3 movement = fwd * accelInput * finalAcceleration * ((m_HasCollision || GroundPercent > 0.0f) ? 1.0f : 0.0f);
 
-        //jump management
-        // should be satisfactory until we add ramps
-        if (jumpHold > 0 && GroundPercent > 0.0f)
-        {
-            JumpCharge = Mathf.Clamp(jumpHold / 120f, 0.5f, 1.0f);
-            Debug.Log("JumpCharge: " + JumpCharge);
-            if(jumpHold > 180)
-            {
-                maxSpeed *= 0.5f;
-            }
-        }
-        if (jump && GroundPercent > 0.0f)
-        {
-            Debug.Log("JumpForce * JumpCharge: " + JumpForce * JumpCharge);
-            Rigidbody.AddForce(Vector3.up * (JumpForce * JumpCharge), ForceMode.Impulse);
-        }
+        //jump
+        maxSpeed = Jump(jumpHold, jump, maxSpeed);
 
         // forward movement
         bool wasOverMaxSpeed = currentSpeed >= maxSpeed;
