21ee7f0e63c720023aeee653df90856b08486caf
James Bui <jtbui20@gmail.com>
2025-04-15 10:57:43 +1000

    fix: Remove serialization for now

--- Assets/Scripts/RacerInformation.cs
+++ Assets/Scripts/RacerInformation.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
-using UnityEngine.TextCore.Text;
 
 // questionable, find examples elsewhere or run by James
-[System.Serializable]
 public struct RacerInformation
 {
     public BaseVehicle VehicleSelected;
