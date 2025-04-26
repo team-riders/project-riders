97337d400afd633c792eaf714eb3056c7da7ac63
AppelJoosh <joshcauson@gmail.com>
2025-04-26 17:49:23 +1000

    remove AddPowerup()

--- Assets/Scripts/RidersGame/VehicleSystems/BaseVehicle.cs
+++ Assets/Scripts/RidersGame/VehicleSystems/BaseVehicle.cs
@@ -132,7 +132,6 @@ namespace RidersRuntime.VehicleSystem
         #endregion
 
         // methods
-        public void AddPowerup(StatPowerup statPowerup) => m_ActivePowerupList.Add(statPowerup);
         public void SetCanMove(bool move) => m_CanMove = move;
         public float GetMaxSpeed() => Mathf.Max(m_FinalStats.TopSpeed, m_FinalStats.ReverseSpeed);
 
