c03bdda40fdbe88084abf1956e4d508a89ac09fe
cofiecofie <103991411@student.swin.edu.au>
2025-06-24 12:50:06 +1000

    Tram doesn't fly anymore, turning added
    Turning needs more refinement (it kinda sucks) the tram doesnt fly at least. Need to implement reverse cycling through nodes when flip is changed

--- Assets/Scripts/RidersGame/AI/Entities/EntityAIInput.cs
+++ Assets/Scripts/RidersGame/AI/Entities/EntityAIInput.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using RidersRuntime.VehicleSystem;
+using System.Data;
 
 namespace RidersRuntime.Input
 {
@@ -21,15 +22,15 @@ public class EntityAIInput : MonoBehaviour
     private Node currentJumpNode;
     [SerializeField] private bool flip = true;
     [SerializeField] private bool flipflag = false;
-    void Awake()
-    {
-        EntityRigidbody = GetComponent<Rigidbody>();
-    }
+        void Awake()
+        {
+            EntityRigidbody = GetComponent<Rigidbody>();
+        }
 
         void OnDrawGizmos()
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawLine(EntityRigidbody.position, EntityRigidbody.transform.forward * 50f);
+            Gizmos.DrawLine(EntityRigidbody.position, EntityRigidbody.transform.forward * -50f);
         }
         ActorInputData inputData = new();
         void Update()
@@ -38,34 +39,41 @@ public class EntityAIInput : MonoBehaviour
 
             targetNode = path.nodes[currentNodeIndex];
             SetTargetPosition(targetNode.position);
-            if (Vector3.Distance(this.transform.position, targetNode.position) < nodeReachThreshold)
+            if (Vector3.Distance(EntityRigidbody.transform.position, targetNode.position) < nodeReachThreshold)
             {
                 if (flipflag) { flip = !flip; }
                 currentNodeIndex = (currentNodeIndex + 1) % path.nodes.Length;
                 if (currentNodeIndex == path.nodes.Length - 1) { FlipFlag(); }
             }
-
-            Vector3 diffToTarget = targetPosition - this.transform.position;
-            Vector3 forward = this.transform.forward;
-
+            Vector3 forward = Vector3.zero;
+            Vector3 diffToTarget = (targetPosition - EntityRigidbody.transform.position);
+            if (flip) { forward = EntityRigidbody.transform.forward; }
+            else { forward = -EntityRigidbody.transform.forward; }
+            
             distanceToTarget = diffToTarget.magnitude;
             float angleToTarget = Vector3.SignedAngle(forward, diffToTarget, Vector3.up);
-
             float turnInput = Mathf.Clamp(angleToTarget / 45f, -1f, 1f);
+            Vector3 turnVector = Vector3.right * turnInput;
             float accelerate = distanceToTarget > nodeAccelerateThreshold ? 1f : 0f;
             float brake = distanceToTarget < nodeCloseThreshold ? 1f : 0f;
             float speed = flip ? -15f : 15f;
+            EntityRigidbody.AddTorque(turnVector * 3f, ForceMode.Acceleration);
             EntityRigidbody.linearVelocity = new(accelerate * speed, 0, 0);
     }
 
-    private void FlipFlag()
-    {
-        flipflag = !flipflag;    
-    }
-
-    public void SetTargetPosition(Vector3 targetPosition)
+        void FixedUpdate()
         {
-            this.targetPosition = targetPosition;
+            EntityRigidbody.AddForce(Physics.gravity * 25f, ForceMode.Acceleration);   
         }
+
+        private void FlipFlag()
+            {
+                flipflag = !flipflag;
+            }
+
+        public void SetTargetPosition(Vector3 targetPosition)
+            {
+                this.targetPosition = targetPosition;
+            }
 }
 }
