ddaf2d3a655936cb0d03b71e17f842412326bf17
AppelJoosh <joshcauson@gmail.com>
2025-07-26 23:11:31 +1000

    Made tutorial text switch based on controller
    also removed erroneous frog3 file and moved rdr-0010 to appropriate filepath

--- Assets/Scripts/RidersGame/GameSystems/Tutorial/TutorialText.cs
+++ Assets/Scripts/RidersGame/GameSystems/Tutorial/TutorialText.cs
@@ -2,15 +2,47 @@ using RidersRuntime.Input;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XInput;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 namespace RidersRuntime
 {
+
     public class TutorialText : MonoBehaviour
     {
 
         public TutorialManager tutorialManager;
         public TextMeshProUGUI text;
+
+        // inputs used in text
+        string Accelerate = "W";
+        string Reverse = "S";
+        string Turn = "A and D";
+        string Jump = "Space";
+        string Boost = ".";
+
+        //KBM inputs
+        string KBMAccelerate = "W";
+        string KBMReverse = "S";
+        string KBMTurn = "A and D";
+        string KBMJump = "Space";
+        string KBMBoost = ".";
+
+        //PlayStation inputs
+        string PSAccelerate = "Forward";
+        string PSReverse = "Backward";
+        string PSTurn = "Left and Right";
+        string PSJump = "X";
+        string PSBoost = "L1";
+
+        //XBOX inputs
+        string XBAccelerate = "Forward";
+        string XBReverse = "Backward";
+        string XBTurn = "Left and Right";
+        string XBJump = "A";
+        string XBBoost = "LB";
+
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
@@ -21,36 +53,66 @@ namespace RidersRuntime
         void Update()
         {
 
+            // this is disgusting
+            ChangeInputText(UnityEngine.Input.GetJoystickNames());
 
-            // this part i feel like could be better but honestly i can't be assed thinking of a better way
-            // enables appropriate inputs at step
             if (tutorialManager.popUpIndex == 0)
             {
-                text.SetText("Hold W to move forward");
+                text.SetText("Hold " + Accelerate + " to move forward");
             }
             if (tutorialManager.popUpIndex == 1)
             {
-                text.SetText("Hold S to brake and move backwards"); 
+                text.SetText("Hold " + Reverse + " to brake and move backwards"); 
             }
             if (tutorialManager.popUpIndex == 2)
             {
-                text.SetText("Use A and D to turn\r\nFollow the path!");
+                text.SetText("Use " + Turn + " to turn\r\nFollow the path!");
             }
             if (tutorialManager.popUpIndex == 3)
             {
-                text.SetText("Hold Space to jump\r\nHold it for longer to jump higher!");
+                text.SetText("Hold " + Jump + " to jump\r\nHold it for longer to jump higher!");
             }
             if (tutorialManager.popUpIndex == 4)
             {
-                text.SetText("Jump on grind rails \r\nto start grinding\r\nHold W to move faster\r\nHold S to move slower");
+                text.SetText("Jump on grind rails \r\nto start grinding\r\nHold " + Accelerate + " to move faster\r\nHold " + Reverse + " to move slower");
             }
             if (tutorialManager.popUpIndex == 5)
             {
-                text.SetText("Press . to boost");
+                text.SetText("Press " + Boost + " to boost");
             }
             if (tutorialManager.popUpIndex == 6)
             {
                 text.SetText("Congrats on completing the tutorial!\r\nReturn to the center checkpoint to leave.");
+            }
+        }
+
+        void ChangeInputText(string[] inputs)
+        {
+            Debug.Log(inputs[0]);
+            Debug.Log(inputs[0] == "Wireless Controller");
+            if (inputs[0] == "Wireless Controller")
+            {
+                Accelerate = PSAccelerate;
+                Reverse = PSReverse;
+                Turn = PSTurn;
+                Jump = PSJump;
+                Boost = PSBoost;
+            }
+            else if (inputs[0] == "XBox Controller")
+            {
+                Accelerate = XBAccelerate;
+                Reverse = XBReverse;
+                Turn = XBTurn;
+                Jump = XBJump;
+                Boost = XBBoost;
+            }
+            else
+            {
+                Accelerate = KBMAccelerate;
+                Reverse = KBMReverse;
+                Turn = KBMTurn;
+                Jump = KBMJump;
+                Boost = KBMBoost;
             }
         }
     }
